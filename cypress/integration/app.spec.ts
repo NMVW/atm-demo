a8b28b7bd3f1a0cf59cb0e02ff4d33e4b40f3181
Nick Vinson <nmvinson@gmail.com>
2021-02-14 20:25:13 -0700

    scaffold txn table ui to implement api integration against

--- cypress/integration/app.spec.ts
+++ cypress/integration/app.spec.ts
@@ -7,10 +7,37 @@ describe("App", function() {
   });
 
   it("renders the app", function() {
-    // how to point to env vars?
-    cy.get(".AccountBalance").should("contain", "$2,000.00");
+
+    cy.get(".AccountBalance").should("contain", "$713.12");
     cy.get(".WithdrawalForm").should("contain", "$0.00");
-    cy.get(".TxnTable").should("contain", "No rows");
+
+    cy.get(".TxnTable").should('exist');
   });
 
+  it("should show 5 most recent txns", function() {
+    cy.get(".TxnTable .MuiDataGrid-row").as('rows');
+
+    cy.get("@rows").first().should("contain", "9");
+    cy.get("@rows").first().should("contain", "9:30 Club");
+    cy.get("@rows").first().should("contain", "$30.00");
+
+    cy.get("@rows").first().next().should("contain", "8");
+    cy.get("@rows").first().next().should("contain", "Arlington Utilities");
+    cy.get("@rows").first().next().should("contain", "$120.00");
+
+    cy.get("@rows").first().next().next().should("contain", "7");
+    cy.get("@rows").first().next().next().should("contain", "Trader Joe's");
+    cy.get("@rows").first().next().next().should("contain", "$67.00");
+
+    cy.get("@rows").first().next().next().next().should("contain", "6");
+    cy.get("@rows").first().next().next().next().should("contain", "7 Eleven");
+    cy.get("@rows").first().next().next().next().should("contain", "$15.46");
+
+    cy.get("@rows").first().next().next().next().next().should("contain", "5");
+    cy.get("@rows").first().next().next().next().next().should("contain", "Sweetgreen");
+    cy.get("@rows").first().next().next().next().next().should("contain", "$11.50");
+
+    cy.get("@rows").first().next().next().next().next().next().should("not.exist");
+
+  })
 });
